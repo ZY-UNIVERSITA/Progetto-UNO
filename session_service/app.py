827dbf218d0733d05c5e5acbf4619fddbfceeee2
ZY <yuhang.zhu@studio.unibo.it>
2025-07-13 18:37:58 +0200

    Add logging package

--- session_service/app.py
+++ session_service/app.py
@@ -1,6 +1,11 @@
 import redis
 import json
 import time
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
+
+logging.info("App started")
 
 STREAM_NAME: str = "game_start_stream"
 GROUP_NAME: str = "game_start_group"
@@ -17,13 +22,13 @@ def process_game(data):
     data_json = json.loads(data)
     lobby_id = data_json['lobby_id']
     players = data_json['players']
-    print(f"Avvio partita per lobby {lobby_id} con giocatori {players}")
+    logging.info(f"Avvio partita per lobby {lobby_id} con giocatori {players}")
 
 while True:
     messages = redis_client.xreadgroup(GROUP_NAME, CONSUMER_NAME, {STREAM_NAME: '>'}, count=1, block=30000)
     for stream, msgs in messages:
         for msg_id, msg_data in msgs:
-            print("messages arrived")
+            logging.info("messages arrived")
             process_game(msg_data)
             redis_client.xack(STREAM_NAME, GROUP_NAME, msg_id)
 
